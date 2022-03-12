d9f1a94e12338475338138eff71acc28e0161b7b
Joss Lee <58053782+josslee@users.noreply.github.com>
2022-03-12 17:41:43 -0500

    finished video call
    finished video call

--- frontend/src/containers/VideoChatPage/index.tsx
+++ frontend/src/containers/VideoChatPage/index.tsx
@@ -2,32 +2,60 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import EVENTS from "../../constants/events";
 import "./index.scss";
+import Video from "../../components/Video";
 
-type VideoChatPageProps = {};
-const socketUrl = "/";
 
-const VideoChatPage = ({}: VideoChatPageProps) => {
-  const [socket, setSocket] = useState<Object | null>(null);
-  const [users, setUsers] = useState(false);
-  const initSocket = () => {
-    let socket = io(socketUrl);
-    setSocket({ socket });
-    socket.on("connect", () => console.log("Connected"));
-    // socket.on(EVENTS.LOGOUT, () => {
-    //   setUsers(false);
-    // });
-    // socket.on(EVENTS.LOGOUT, () => {
-    //   setUsers(false);
-    // });
-  };
-  useEffect(() => {}, []);
+import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+import VideoPlayer from '../../components/VideoCall/VideoPlayer';
+import Sidebar from '../../components/VideoCall/SideBar';
+import Notifications from '../../components/VideoCall/Notifications';
+
+import { ContextProvider } from '../../contexts/SocketContext';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
+const VideoChatPage = () => {
+  const classes = useStyles();
   return (
+    <ContextProvider>
     <div className="ms-camera">
       <div className="ms-camera__section">
-        <h1>Video Chat Page</h1>
+      <Typography variant="h2" align="center">Video Chat</Typography>
+      <VideoPlayer />
+      <Sidebar>
+        <Notifications />
+      </Sidebar>
       </div>
     </div>
+    </ContextProvider>
   );
 };
 
