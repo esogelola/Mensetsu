5c6759c89f4613b364c7a84cf520292d5c282837
Davin-Ricketts <davinricketts32@gmail.com>
2022-03-13 10:23:50 -0400

    Add moniter to landing page

--- frontend/src/components/Description/index.tsx
+++ frontend/src/components/Description/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './index.scss'
+import {ReactComponent as Image} from '../../assets/image 38.svg'
 
 function Description() {
   return (
     <div className='product-description'>
       <span>Learn. Practice. Interview.</span>
       <p>
-        gjfdsklgjfsdlk ;jkfld; jklgjkl;dsasjffdakjsl;jakdl ;dasjfkl ;fjkl;a dj;kl dsjklf; dasjjkflads;fjdsak lfhdsajklfdsah jdsfa hjkladsfh jklfdhs ajkladsf hjkldfsa hjkdafs hjkl dfshajkldsf ahjkl ahjkladfs hjkl hjkldfs ahj dfksalfdsah bjkl dfshjkladfs hjlkfads hjla dfshjklafds hfjk dsal
+        Gain an edge with Mensetsu's video interview practice tool tailored to students studying computer science. Get instant feedback on your tone and communication. You can also practice your interview skills with a real interviewer.
       </p>
+      <Image className='description-image'/>
     </div>
   )
 }
