1572355f651b9df78f673cb3193031204689166c
esogelola <esogelola@gmail.com>
2022-03-13 03:55:32 -0400

    Finished App Page

--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,32 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar, Button } from "@mui/material";
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 import "./index.scss";
 import * as ROUTES from "../../constants/routes";
 
-
-const barStyle ={ 
-  background: '#F5EEEE'
- };
- const linkStyle={
-  textTransform: 'lowercase'
- };
+const barStyle = {
+  background: "#F5EEEE",
+};
+const linkStyle = {
+  textTransform: "lowercase",
+};
 
 const Navigation = () => {
   return (
     <AppBar style={barStyle} className="ms-nav" position="static">
       <Toolbar>
-      <b className="ms-nav__logo">
-        MENSETSU
-      </b>
-      <small className="ms-nav__stacked">
-        Interview Practicing Platform
-      </small>
-      <Typography sx={{ flexGrow: 1 }}></Typography>
-        <Button color="inherit" >
+        <Link className="ms-nav__logo" to={ROUTES.LANDING}>
+          MENSETSU
+        </Link>
+        <small className="ms-nav__stacked">Interview Practicing Platform</small>
+        <Typography sx={{ flexGrow: 1 }}></Typography>
+        <Button color="inherit">
           <Link style={linkStyle} className="ms-nav__link" to={ROUTES.APP}>
             home
           </Link>
