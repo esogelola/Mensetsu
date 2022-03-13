1ce915e372ecf5856737cfce944badb5ae940af7
esogelola <esogelola@gmail.com>
2022-03-13 16:11:47 -0400

    Updated Navbar

--- frontend/src/containers/VideoChatPage/index.tsx
+++ frontend/src/containers/VideoChatPage/index.tsx
@@ -6,7 +6,7 @@ import Video from "../../components/Video";
 
 import { Grid, Typography, AppBar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { ListItem } from '@mui/material'
+import { ListItem } from "@mui/material";
 import VideoPlayer from "../../components/VideoCall/VideoPlayer";
 import Sidebar from "../../components/VideoCall/SideBar";
 import Notifications from "../../components/VideoCall/Notifications";
@@ -17,49 +17,39 @@ import Navigation from "../../components/Navigation";
 const VideoChatPage = () => {
   return (
     <div className="ms-camera">
-    <div className="ms-camera__section">
-    <ContextProvider>
-
-    <Grid container className="pageGrid">
-    <Grid item xs={7}>
-      <Grid container className="gridContainer2">
-        <Grid item xs={12}>
-
-          <VideoPlayer />
-        </Grid> 
-        <Grid item xs={12}>
-        <Sidebar>
-            
-          </Sidebar>
-        </Grid> 
-        <Grid item xs={7}>
-
-        <div className="ms-setsu__container containerWide ">
-            <p className="containerHeading">Transcription</p>
-            <p> 
-            </p>
-          </div>
-        </Grid>
-        <Grid item xs={4}>
-        <div className="ms-setsu__container containerWide ">
-        <Notifications />
-        </div>
-        </Grid>
-
-        </Grid>
-      
-    </Grid>
-    <Grid item xs={4}>
-    <div className="ms-setsu__container containerCode ">
-            <p className="containerHeading">Codeshare</p>
-            <p> 
-            </p>
-          </div>
-    </Grid>
-    </Grid>
-    </ContextProvider>
-    </div>
+      <Navigation />
+      <div className="ms-camera__section">
+        <ContextProvider>
+          <Grid container className="pageGrid">
+            <Grid item xs={7}>
+              <Grid container className="gridContainer2">
+                <Grid item xs={12}>
+                  <VideoPlayer />
+                </Grid>
+                <Grid item xs={12}></Grid>
+                <Grid item xs={7}>
+                  <div className="ms-setsu__container containerWide ">
+                    <p className="containerHeading">Transcription</p>
+                    <p></p>
+                  </div>
+                </Grid>
+                <Grid item xs={4}>
+                  <div className="ms-setsu__container containerWide ">
+                    <Notifications />
+                  </div>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={4}>
+              <div className="ms-setsu__container containerCode ">
+                <p className="containerHeading">Codeshare</p>
+                <p></p>
+              </div>
+            </Grid>
+          </Grid>
+        </ContextProvider>
       </div>
+    </div>
   );
 };
 
